21ba39c095c481faf911f24076e212f84ac29be0
muhammadveral <muhammadveral02@gmail.com>
2022-07-15 21:09:40 +0700

    User Routes & Controller

---
+++ backend/controllers/userController.js
@@ -0,0 +1,26 @@
+// @desc    Register user
+// @route   POST /api/users
+// @access  Public
+const registerUser = (req, res) =>{
+    res.json({message: "Register User"})
+}
+// @desc    Login user
+// @route   POST /api/users
+// @access  Public
+const loginUser = (req, res) =>{
+    res.json({message: "Login User"})
+}
+// @desc    Get user data
+// @route   POST /api/users
+// @access  Public
+const getMe = (req, res) =>{
+    res.json({message: "User data display"})
+}
+
+
+module.exports = {
+    registerUser,
+    loginUser,
+    getMe
+
+} 
