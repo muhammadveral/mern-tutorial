82b187e2567201e7ba5e85f97f9cd2baef111e70
muhammadveral <muhammadveral02@gmail.com>
2022-07-15 21:05:33 +0700

    Add User to goal model

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose')
 const goalSchema = mongoose.Schema(
-    {
+    {              
+    
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'User',
+        },
 
-        
         text :{
             type: String,
             required: [true, 'Please add a text value']
@@ -12,5 +17,4 @@ const goalSchema = mongoose.Schema(
         timestamps : true,
     }
 )
-
-module.exports = mongoose.model('Goal', goalSchema) 
+module.exports = mongoose.model('Goal', goalSchema)
