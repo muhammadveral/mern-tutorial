eab3c082a2774f085ce9af1f1efaff609f2b085b
muhammadveral <muhammadveral02@gmail.com>
2022-07-15 20:31:57 +0700

    Error & Exception

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
 const port = process.env.PORT ||5000
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended : false}))
 
 app.listen(port, ()=> console.log(`Server started on port ${port}`))
 app.get('/api/goals', (req,res) => {
@@ -14,5 +18,7 @@ app.use('/api/goals', require('./routes/goalRoutes'))
 
 app.use(express.json())
 app.use(express.urlencoded({extended : false}))
+
+app.use(errorHandler)
 
 app.listen(port, ()=> console.log(`Server started on port ${port}`))
